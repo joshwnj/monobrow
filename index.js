a8b4bce3338c541ee4910b6d0e1cb4ef0b129b3c
Josh Johnston <josh@x-team.com>
2017-01-06 17:59:57 +1100

    change the way packs and vendor files are collected
    - normalize packs and other config
    - vendor file paths are inferred from pack._path

--- index.js
+++ index.js
@@ -7,16 +7,20 @@ const mkdirp = require('mkdirp')
 const watchify = require('watchify')
 const multistream = require('multistream')
 
-const allDeps = []
-
 module.exports = function (opts) {
   opts = opts || {}
   if (!opts.rootDir) { opts.rootDir = process.cwd() }
   if (!opts.cacheFile) { opts.cacheFile = '.bcache.json' }
   if (typeof opts.inc === 'undefined') { opts.inc = true }
-  if (!opts.outFile) {
-    opts.outFile = path.join(opts.rootDir, opts.outDir, path.basename(opts.entry))
+
+  const outputDefaults = {
+    dir: 'dist',
+    bundle: path.basename(opts.entry),
+    vendor: '_vendor.js'
   }
+
+  opts.output = Object.assign({}, opts.output || {}, outputDefaults)
+  opts.outFile = path.join(opts.rootDir, opts.output.dir, opts.output.bundle)
 
   var b
   if (opts.watch) {
@@ -30,12 +34,20 @@ module.exports = function (opts) {
 
   var bundle = require('./lib/build')(b, opts)
 
-  // setup (optional) can either be a function or an array of functions
-  if (typeof opts.setup === 'function') {
-    opts.setup(b, opts)
-  } else if (Array.isArray(opts.setup)) {
-    opts.setup.forEach(function (f) { f(b, opts) })
-  }
+  // normalize setup packs
+  const packs = normalizePacks(opts.packs)
+  packs.forEach((p) => {
+    // ignore vendor deps in the bundle
+    b.external(p.vendor)
+
+    // run the setup
+    p.setup(b, opts)
+  })
+
+  // collect pre-bundled vendor files for all packs
+  const vendorFiles = packs.map((p) => {
+    return p._path ? path.join(path.dirname(p._path), 'dist', 'vendor.js') : null
+  }).filter(Boolean)
 
   // make sure the output directory exists
   var outDir = path.dirname(opts.outFile)
@@ -55,32 +67,72 @@ module.exports = function (opts) {
     }
   }
 
-  const bundleFunc = bundle.bind(null, onFinish)
-
-  if (!allDeps.length) { return bundleFunc() }
-
-  concatDeps(opts, allDeps, function (err) {
+  concatVendorFiles(opts, vendorFiles, function (err) {
     if (err) {
       console.log(err)
     }
 
-    bundleFunc()
+    bundle(onFinish)
   })
 }
 
-module.exports.deps = function (b, deps, outFile) {
-  b.external(deps)
-  allDeps.push(outFile)
-// define a pack
+/*
+
+Register a pack
+
+*/
 module.exports.pack = function (opts) {
   opts._path = caller()
   return opts
 }
 
-function concatDeps (opts, deps, cb) {
+/*
+
+Concat all vendor files
+
+*/
+function concatVendorFiles (opts, deps, cb) {
+  if (!deps.length) { return cb() }
+
   const streams = deps.map(d => fs.createReadStream(d))
   multistream(streams)
-    .pipe(fs.createWriteStream(path.join(opts.outDir, '_deps.js')))
+    .pipe(fs.createWriteStream(path.join(opts.rootDir, opts.output.dir, opts.output.vendor)))
     .on('error', cb)
     .on('close', () => cb(null))
+}
+
+/*
+
+Normalize a single pack
+
+*/
+function normalizePack (input) {
+  const setup = (typeof input === 'function') ? input : input.setup
+  const vendor = input.vendor || {}
+  vendor.modules = vendor.modules || []
+
+  return {
+    _path: input._path,
+    setup: setup,
+    vendor: vendor,
+  }
+}
+
+/*
+
+Get a normalized array of setup packs
+
+*/
+function normalizePacks (input) {
+  if (!input) { return [] }
+
+  // input can either be:
+  // - a function
+  // - an array of functions
+  // - an array of { setup: function } (as is often the case with packs)
+  if (typeof input === 'function') {
+    return [ normalizePack(input) ]
+  }
+
+  return input.map(normalizePack)
 }
